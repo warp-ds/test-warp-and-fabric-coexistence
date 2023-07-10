7067e70d5bc733ae9122996f75984909ab59d5c3
Silje Christensen <silje.christensen@cognite.com>
2023-07-10 12:17:07 +0200

    fix: clean up html tags examples

--- src/components/test-resets/index.tsx
+++ src/components/test-resets/index.tsx
@@ -2,8 +2,8 @@ const TestResets = () => {
   return (
     <div>
       <p>Clickable button element (button):</p>
-      <button>Click me</button>
-      <p>anchor</p>
+      <button onClick={() => console.log('clicked')}>Click me</button>
+      <p>(anchor):</p>
       <a href="#">Anchor Link</a>
 
       <hr/>
@@ -20,7 +20,25 @@ const TestResets = () => {
       <b>Bold content</b>
       <p>Strong text (strong):</p>
       <strong>Strong content</strong>
+      <p>Summary for a details/accordion element(summary):</p>
+      <summary>Summary content</summary>
+      <p>Block-level quotation (blockquote):</p>
+      <blockquote>
+        <p>Quoted content:</p>
+        <footer>Author</footer>
+      </blockquote>
+      <p>Inline element for indicating computer code (code):</p>
+      <code>&lt;code&gt; HTML tag</code>
+      <p>Keyboard input (kbd):</p>
+      <kbd>Keyboard input content</kbd>
+      <p>Sample output (samp):</p>
+      <samp>Sample output content</samp>
+      <p>Subscript text (sub):</p>
+      <sub>Subscript content</sub>
+      <p>Superscript text (sup):</p>
+      <sup>Superscript content</sup>
 
+      <hr/>
 
       <p>Single-line text input field (input):</p>
       <input type="text" placeholder="Enter your name"/>
@@ -29,41 +47,24 @@ const TestResets = () => {
 
       <hr/>
 
-      <p>Inline element for indicating computer code (code):</p>
-      <code>&lt;code&gt; HTML tag</code>
-
-      <p>Keyboard input (kbd):</p>
-      <kbd>Keyboard input content</kbd>
-
-      <p>Sample output (samp):</p>
-      <samp>Sample output content</samp>
-
-      <p>Subscript text (sub):</p>
-      <sub>Subscript content</sub>
-
-      <p>Superscript text (sup):</p>
-      <sup>Superscript content</sup>
-
-      <hr/>
-
       <p>Option group within a select element (optgroup):</p>
       <optgroup label="Group 1">
         <option>Option 1</option>
         <option>Option 2</option>
       </optgroup>
-
-
       <p>Progress indicator (progress):</p>
       <progress value="50" max="100"></progress>
+      <p>Figure element, used for images and captions (figure):</p>
+      <figure>
+        <img src="../../assets/dogs-unsplash.jpg" alt="Image description"/>
+        <figcaption>Caption for the image</figcaption>
+      </figure>
+      <p>Fieldset for grouping form elements (fieldset) :</p>
+      <fieldset>
+        <legend>Legend for the fieldset</legend>
+      </fieldset>
 
-      <p>Summary for a details/accordion element:</p>
-      <summary>Summary content</summary>
-
-      <p>Block-level quotation (blockquote):</p>
-      <blockquote>
-        <p>Quoted content:</p>
-        <footer>Author</footer>
-      </blockquote>
+      <hr/>
 
       <p>Definition list (dl):</p>
       <dl>
@@ -72,41 +73,21 @@ const TestResets = () => {
         <dt>Term 2</dt>
         <dd>Definition 2</dd>
       </dl>
-
-      <p>Figure element, used for images and captions (figure):</p>
-      <figure>
-        <img src="../../assets/dogs-unsplash.jpg" alt="Image description"/>
-        <figcaption>Caption for the image</figcaption>
-      </figure>
-
-
-
-      <p>Fieldset for grouping form elements (fieldsest) :</p>
-      <fieldset>
-        <legend>Legend for the fieldset</legend>
-        <p> test test :</p>
-      </fieldset>
-
-      <hr/>
-
       <p>Ordered list (ol):</p>
       <ol>
         <li>List item 1</li>
         <li>List item 2</li>
       </ol>
-
       <p>Unordered list (ul):</p>
       <ul>
         <li>List item 1</li>
         <li>List item 2</li>
       </ul>
-
       <p>Menu list (menu):</p>
       <menu>
         <li>Menu item 1</li>
         <li>Menu item 2</li>
       </menu>
-
     </div>
 
   )
