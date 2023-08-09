38b27f59804451d938b90d716db3e368238823a9
Silje Christensen <silje.christensen@cognite.com>
2023-08-09 11:37:32 +0200

    fix: bump warp packages and add uno.css import back

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 // Project specific styling
 import './index.css'
-
+import 'uno.css'
 /**
  * uno.css contains the uno generated css based on what classes that are used in this project.
  * Because we have set the skipResets flag in uno.config.js, the warp reset is not included here.
